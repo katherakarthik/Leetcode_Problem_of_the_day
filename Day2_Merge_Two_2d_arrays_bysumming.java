89b45f99bd88041fd245f35bcb0a3116c4298497
karthikkathera <120442160+katherakarthik@users.noreply.github.com>
2025-03-02 15:28:28 +0530

    Create Day2_Merge_Two_2d_arrays_bysumming.java

---
+++ Day2_Merge_Two_2d_arrays_bysumming.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        List<int[]>res=new ArrayList<>();
+        int p1=0,p2=0;
+        int m=nums1.length;
+        int n=nums2.length;
+        while(p1<m&&p2<n){
+            if(nums1[p1][0]==nums2[p2][0]){
+                res.add(new int[]{nums1[p1][0],nums1[p1][1]+nums2[p2][1]});
+                p1++;
+                p2++;
+            }
+            else if(nums1[p1][0]<nums2[p2][0]){
+                res.add(nums1[p1]);
+                p1++;
+            }
+            else{
+                res.add(nums2[p2]);
+                p2++;
+            }
+        }
+        while(p1<m){
+            res.add(nums1[p1]);
+            p1++;
+        }
+        while(p2<n){
+            res.add(nums2[p2]);
+            p2++;
+        }
+        int result[][]=new int[res.size()][2];
+        for(int i=0;i<res.size();i++){
+            result[i]=res.get(i);
+        }
+        return result;
+    }
+}
