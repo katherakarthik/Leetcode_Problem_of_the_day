5ce5b2e0c5a540355944b2edc20a94dbc2654cb0
karthikkathera <120442160+katherakarthik@users.noreply.github.com>
2025-03-07 12:24:18 +0530

    Update Day_7_closest_prime_no.java

--- Day_7_closest_prime_no.java
+++ Day_7_closest_prime_no.java
@@ -32,3 +32,63 @@ class Solution {
         return res;
     }
 }
+
+//Second method
+import java.util.*;
+
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        // Initialize the result array
+        int[] res = new int[]{-1, -1};
+        int minDiff = Integer.MAX_VALUE;
+
+        // Generate all prime numbers in the range [left, right]
+        List<Integer> primes = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if (isPrime(i)) {
+                primes.add(i);
+            }
+        }
+
+        // Find the pair of consecutive primes with the smallest difference
+        for (int i = 1; i < primes.size(); i++) {
+            int diff = primes.get(i) - primes.get(i - 1);
+            if (diff < minDiff) {
+                minDiff = diff;
+                res[0] = primes.get(i - 1);
+                res[1] = primes.get(i);
+            }
+        }
+
+        return res;
+    }
+
+    // Helper function to check if a number is prime
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
