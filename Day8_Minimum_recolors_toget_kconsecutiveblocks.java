9f81881baac679ad1482128423b553c83ba7e47c
karthikkathera <120442160+katherakarthik@users.noreply.github.com>
2025-03-08 12:54:53 +0530

    Create Day8_Minimum_recolors_toget_kconsecutiveblocks.java

---
+++ Day8_Minimum_recolors_toget_kconsecutiveblocks.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int w=0;
+        for(int i=0;i<k;i++){
+            if(blocks.charAt(i)=='W')w++;
+        }
+        int res=w;
+        int n=blocks.length();
+        for(int i=1;i<(n-k+1);i++){
+            if(blocks.charAt(i-1)=='W')w--;
+            if(blocks.charAt(i+k-1)=='W')w++;
+            res=Math.min(res,w);
+        }
+        // res=Math.min(res,w);
+        return res;
+    }
+}
